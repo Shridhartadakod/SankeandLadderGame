4eda542552f8a82dcbcb1427e7229c7dbe531aea
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-10 18:15:47 +0530

    Player Position is addded

--- SnakeandLadder1/SnakeandLadderGame.cs
+++ SnakeandLadder1/SnakeandLadderGame.cs
@@ -9,6 +9,7 @@ namespace SnakeandLadder1
     internal class SnakeandLadderGame
     {
         int position;
+        Random random=new Random();
 
         public SnakeandLadderGame()
         {
@@ -18,4 +19,30 @@ namespace SnakeandLadder1
         {
             Console.WriteLine("Position is :" + position);
         }
+
+        public void RollDie()
+        {
+            int roll = random.Next(1, 7);
+            Console.WriteLine("Rolled: " + roll);
+            position += roll;
+            Derive();
+        }
+
+
+        public void Option(int roll)
+        {
+            int option=random.Next(0, 3);
+            switch (option)
+            {
+                case 0: Console.WriteLine("no play");
+                    break;
+                case 1: Ladder1(roll);
+                    break;
+                case 2:
+                    Sanke(roll);
+                    break ;
+
+            }
+        }
+    }
 }
