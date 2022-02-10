65952d48c2072bf392843ab41588dddd9969483b
Shridhartadakod <shridhar1491998@gmail.com>
2022-02-10 17:42:56 +0530

    Added Initial Position

---
+++ SnakeandLadder1/SnakeandLadderGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder1
+{
+    internal class SnakeandLadderGame
+    {
+        int position;
+
+        public SnakeandLadderGame()
+        {
+            position = 0;
+        }
+        public void Derive()
+        {
+            Console.WriteLine("Position is :" + position);
+        }
+}
